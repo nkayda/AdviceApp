1c0a22963da6d1b7685c8efb9f730e182d74a9af
Natalie Kayda <nataliekayda708@gmail.com>
2025-03-09 01:22:44 -0800

    final touches

--- App.js
+++ App.js
@@ -1,35 +1,28 @@
-import { StatusBar } from 'expo-status-bar';
-import { Pressable, StyleSheet, Text, View, Button } from 'react-native';
+import { Pressable, StyleSheet, Text, View, Button, Image } from 'react-native';
 import { useState } from 'react';
 import { useFonts } from 'expo-font';
 import { Quicksand_600SemiBold } from '@expo-google-fonts/quicksand';
 import * as ImagePicker from 'expo-image-picker';
 
+/* 
+
+SOURCES:
+- https://icanhazdadjoke.com/api for the API docs
+- https://developer.mozilla.org/en-US/docs/Web/API/Fetch_API/Using_Fetch for using headers
+- https://blog.logrocket.com/adding-custom-fonts-react-native/ for loading custom fonts
+- https://docs.expo.dev/versions/latest/sdk/imagepicker/ for ImagePicker docs
+
+*/
 
 export default function App() {
   
-  const [activeJoke, setActiveJoke] = useState("i dont have a joke in mind right now...");
+  const [activeJoke, setActiveJoke] = useState("I dont have a joke in mind right now...");
   const [errorMessage, setErrorMessage] = useState("");
   const [image, setImage] = useState(null);
 
   const [fontsLoaded] = useFonts({
     Quicksand_600SemiBold,
   });
-
-  const pickImage = async () => {
-    let result = await ImagePicker.launchImageLibraryAsync({
-      mediaTypes: ['images'],
-      allowsEditing: true,
-      aspect: [3, 3],
-      quality: 1,
-    });
-
-    console.log(result);
-
-    if (!result.canceled) {
-      setImage(result.assets[0].uri);
-    }
-  };
 
   const fetchAdvice = async () => {
     
@@ -47,10 +40,22 @@ export default function App() {
     catch (err) {
       console.log(err);
     }
-
-
-
   }
+
+  const pickImage = async () => {
+    let result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ['images'],
+      allowsEditing: true,
+      aspect: [3, 3],
+      quality: 1,
+    });
+
+    console.log(result);
+
+    if (!result.canceled) {
+      setImage(result.assets[0].uri);
+    }
+  };
   
   return (
     <View style={styles.container}>
@@ -58,6 +63,8 @@ export default function App() {
       <View style={styles.jokeContainer}>
         <Text style={styles.jokeText}>{activeJoke}</Text>
       </View>
+
+      {image && <Image source={{ uri: image }} style={styles.image} />}
       
       <Pressable style={styles.button} onPress={fetchAdvice}>
         <Text style={styles.buttonText}>
@@ -67,7 +74,6 @@ export default function App() {
 
       <View>
         <Button title="Pick an image from camera roll" onPress={pickImage} />
-        {image && <Image source={{ uri: image }} style={styles.image} />}
       </View>
 
     </View>
@@ -97,7 +103,8 @@ const styles = StyleSheet.create({
   button: {
     backgroundColor: '#363636',
     padding: 14,
-    borderRadius: 50
+    borderRadius: 50,
+    marginBottom: 16,
   },
   buttonText : {
     fontSize: 14,
@@ -108,5 +115,6 @@ const styles = StyleSheet.create({
   image: {
     width: 200,
     height: 200,
+    marginBottom: 16,
   },
 });
